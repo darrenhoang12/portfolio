4214ff915832d4958dab323614f9ee3f64655c53
Darren Hoang <darrenhoang@Darrens-MBP.lan>
2023-07-21 11:55:14 -0700

    Added package names for projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,11 @@ const Projects = () => {
         <div className="projects">
           <a href="https://github.com/darrenhoang12/nba_mvp_predictor" target="_blank" className="nba_mvp project">
             <h2 className="title">MVP Predictor</h2>
+            <ul className="tech">
+              <li>python</li>
+              <li>selenium</li>
+              <li>sk-learn</li>
+            </ul>
             <p className="description">
               Scraped NBA player data and built regression models to accurately predict the MVP of the
               season based purely on stats. 
@@ -15,6 +20,11 @@ const Projects = () => {
           </a>
           <a href="https://github.com/darrenhoang12/Mario_RL" target="_blank" className="mario_rl project">
             <h2 className="title">Mario Agent</h2>
+            <ul className="tech">
+              <li>python</li>
+              <li>pytorch</li>
+              <li>gym</li>
+            </ul>
             <p className="description">
               Trained a reinforcement learning agent that is capable of playing Super Mario Bros entirely
               on its own.
@@ -22,8 +32,15 @@ const Projects = () => {
           </a>
           <a href="https://github.com/darrenhoang12/BPS_multi_labeling" target="_blank"className="bps_multi project">
             <h2 className="title">BPS Microscopy</h2>
+            <ul className="tech">
+              <li>python</li>
+              <li>pytorch</li>
+              <li>wandb</li>
+              <li>aws</li>
+            </ul>
             <p className="description">
-              Description for BPS Microscopy Project
+              Retrieved irradiated mouse cell image data from AWS and implemented a supervised
+              learning model to predict types of radiation and damage incurred.
             </p>
           </a>
         </div>
