52e2ce29906831edf072ab38f92b717ea6b884d9
Darren Hoang <darrenhoang@Darrens-MBP.lan>
2023-08-24 15:40:01 -0700

    Added portrait and updated landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,8 +5,11 @@ const Landing = () => {
   return (
     <div className="section" data-anchor="landing">
       <div className="landing-content">
-        <h1 className="landing-text">darren hoang</h1>
-        <p>software engineer from california</p>
+        <img src="images/portrait.jpeg"/>
+        <div className="landing-text">
+          <h1>darren hoang</h1>
+          <p>software engineer from california</p>
+        </div>
       </div>
     </div>
   );
